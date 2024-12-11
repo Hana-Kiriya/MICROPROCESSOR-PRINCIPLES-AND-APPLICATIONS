ddded9e311a6a2d580678cdc6dcee44b9d2b2d34
Hana-Kiriya <138017691+Hana-Kiriya@users.noreply.github.com>
2024-12-12 00:50:17 +0800

    Update advanced.c

--- lab9.X/advanced.c
+++ lab9.X/advanced.c
@@ -54,29 +54,29 @@ void main(void)
     //configure OSC and port
     OSCCONbits.IRCF = 0b100; //1MHz
     TRISAbits.RA0 = 1;       //RA0: analog input port
-    TRISB = 0x00; //output
-    LATB = 0x00; //clear PORTB
+     TRISB = 0x00; //設為輸出端
+    LATB = 0x00; //clear PORTD
     
     //step1
-    ADCON1bits.VCFG0 = 0;     //?????????
+    ADCON1bits.VCFG0 = 0;       //參考電壓為內部電源
     ADCON1bits.VCFG1 = 0;
-    ADCON1bits.PCFG = 0b1110; //AN0 ?analog input,???? digital
-    ADCON0bits.CHS = 0b0000;  //AN0 ?? analog input
-    ADCON2bits.ADCS = 0b000;  //????000(1Mhz < 2.86Mhz)
-    ADCON2bits.ACQT = 0b001;  //Tad = 2 us acquisition time ? 2Tad = 4 > 2.4
+    ADCON1bits.PCFG = 0b1110; //AN0 為analog input,其他則是 digital
+    ADCON0bits.CHS = 0b0000;  //AN0 當作 analog input
+    ADCON2bits.ADCS = 0b000;  //查表後設000(1Mhz < 2.86Mhz)
+    ADCON2bits.ACQT = 0b001;  //Tad = 2 us acquisition time設2Tad = 4 > 2.4
     ADCON0bits.ADON = 1;
     ADCON2bits.ADFM = 0;    //left justified 
     
     
     //step2
-    PIE1bits.ADIE = 1; //??ADC??
-    PIR1bits.ADIF = 0; //???????
-    INTCONbits.PEIE = 1; //??????
-    INTCONbits.GIE = 1; //??????
+    PIE1bits.ADIE = 1; //啟用ADC中斷
+    PIR1bits.ADIF = 0; //清除中斷標誌位
+    INTCONbits.PEIE = 1; //啟用周邊中斷
+    INTCONbits.GIE = 1; //啟用全域中斷
 
 
     //step3
-    ADCON0bits.GO = 1; //??ADC?????????0
+    ADCON0bits.GO = 1; //啟用ADC，當轉換完成會變成0
     value = (ADRESH << 2) | (ADRESL >> 6); //8 bits
     index = value / 128;
     v_before = value;
